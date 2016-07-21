43219862b5e1a040a3bc5cdefb613219d096936f
Simon Fletcher <Simon Fletcher>
2016-07-21 12:38:23 +0100

    Adding more to the Door script.

---
+++ Assets/scripts/object/DoorProjectileFire.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorProjectileFire : Door {
+	
+	// Update is called once per frame
+	public override void Update () {
+		
+		base.Update ();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if ((hit = other.gameObject.GetComponent<Projectile>()) != null) {
+
+			if (hit.weaponDoorLevel >= doorLevel  && isClosed) {
+
+				doOpening = true;
+
+			}
+
+		}
+
+	}
+}
