c873cbdb45a7deb78a4784e168f20cd202cbfe50
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2016-11-04 16:37:59 +0000

    no message

--- Assets/Scripts/_debug/_DEBUG_CHANGE_LANG.cs
+++ Assets/Scripts/_debug/_DEBUG_CHANGE_LANG.cs
@@ -6,13 +6,17 @@ public class _DEBUG_CHANGE_LANG : MonoBehaviour {
 
 	public void _DEBUG_TOGGLE_LANG() {
 
-		if (LanguageManager.Instance.CurrentlyLoadedCulture.languageCode == "fr") {
+		if (LanguageManager.Instance.CurrentlyLoadedCulture.languageCode == "de") {
 
 			LanguageManager.Instance.ChangeLanguage ("en");
 
 		} else if (LanguageManager.Instance.CurrentlyLoadedCulture.languageCode == "en") {
 
 			LanguageManager.Instance.ChangeLanguage ("fr");
+
+		} else if (LanguageManager.Instance.CurrentlyLoadedCulture.languageCode == "fr") {
+
+			LanguageManager.Instance.ChangeLanguage ("de");
 
 		}
 
