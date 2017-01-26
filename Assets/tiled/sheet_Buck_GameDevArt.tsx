ad4c07c3f98da12e2365ae10e38f8844316940a3
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2017-01-26 19:36:27 +0000

    no message

---
+++ Assets/tiled/sheet_Buck_GameDevArt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sheet" tilewidth="7" tileheight="7" spacing="1" tilecount="2304" columns="48">
+ <image source="../Sprites/geometry/sheet.png" trans="201429" width="384" height="384"/>
+</tileset>
