999e669042fbe3eab36c2350f5d76c7435872568
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2017-01-27 12:30:08 +0000

    HOUSEKEEPING

---
+++ Assets/tiled/Clearwater2.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Clearwater2" tilewidth="77" tileheight="88" tilecount="6" columns="2">
+ <tile id="1">
+  <image width="77" height="88" source="../Sprites/geometry/clearwater_cliffs/clearwater_cliff_0.png"/>
+ </tile>
+ <tile id="2">
+  <image width="69" height="88" source="../Sprites/geometry/clearwater_cliffs/clearwater_cliff_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="61" height="86" source="../Sprites/geometry/clearwater_cliffs/clearwater_cliff_2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="36" height="16" source="../Sprites/geometry/clearwater_cliffs/clearwater_cliff_3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="24" height="12" source="../Sprites/geometry/clearwater_cliffs/clearwater_cliff_4.png"/>
+ </tile>
+ <tile id="6">
+  <image width="18" height="4" source="../Sprites/geometry/clearwater_cliffs/clearwater_cliff_5.png"/>
+ </tile>
+</tileset>
