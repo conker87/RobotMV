25f9ac89039bf83e7d8c5110128a8ce492f0cea2
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2017-02-06 01:00:12 +0000

    Some failed attempts at changing material colours

---
+++ Assets/ChangeSharedMaterialRenderColour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSharedMaterialRenderColour : MonoBehaviour {
+
+	Renderer rend;
+	public Color color;
+
+	// Use this for initialization
+	void Start () {
+	
+		rend = GetComponent<Renderer> ();
+		rend. = color;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rend.sharedMaterial.color = color;
+
+	}
+
+}
