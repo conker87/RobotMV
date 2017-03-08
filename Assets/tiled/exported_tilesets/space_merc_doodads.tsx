8a8e0099df190031fda4fb1619c28c4a3e4df70f
SIMON\Simon Fletcher <sjfletcherhtc@gmail.com>
2017-03-08 14:47:26 +0000

    - Replacing the old space_merc tileset with smaller ones, also replaced the prototype tileset with a smaller one.

---
+++ Assets/tiled/exported_tilesets/space_merc_doodads.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="space_merc_doodads" tilewidth="8" tileheight="8" tilecount="640" columns="10">
+ <image source="../../Sprites/geometry/space_merc/space_merc_doodads.png" width="80" height="384"/>
+</tileset>
